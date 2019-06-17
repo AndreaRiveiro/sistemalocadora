48e763d26619f4c4fe018f0f5a3e823ac0f93294
AndreaRiveiro <49888116+AndreaRiveiro@users.noreply.github.com>
2019-06-17 09:10:46 -0300

    Estrutura Inicial com lavarel

---
+++ resources/views/cadastro.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+@extends("template")
+
+@section("container")
+    <h1>Cadastro</h1>
+    <ul>
+        @forelse($usuarios as $usuario)
+        <li>{{$usuario['nome']}}</li>
+        @empty
+        <li>Não tem usuarios</li>
+        @endforelse
+    </ul>
+@endsection   
